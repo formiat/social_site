beb78db581ac1e56b83bdd430cc3d751cf7e942c
Alexander Aglerimov <formiat95@gmail.com>
2021-11-02 22:14:55 +0300

    formatting

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,27 @@
 #![feature(proc_macro_hygiene, plugin, decl_macro)]
 
-#[macro_use] extern crate rocket;
-extern crate rocket_contrib;
-
-#[macro_use] extern crate diesel;
+#[macro_use]
+extern crate rocket;
+#[macro_use]
+extern crate diesel;
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate serde_json;
 
 extern crate dotenv;
-
 extern crate r2d2;
 extern crate r2d2_diesel;
-
-#[macro_use] extern crate serde_derive;
-#[macro_use] extern crate serde_json;
-
+extern crate rocket_contrib;
 
 use dotenv::dotenv;
-use std::env;
 use routes::*;
+use std::env;
 
-mod schema;
-mod models;
 mod db;
+mod models;
 mod routes;
-
+mod schema;
 
 fn rocket() -> rocket::Rocket {
     dotenv().ok();
@@ -33,13 +32,22 @@ fn rocket() -> rocket::Rocket {
 
     rocket::ignite()
         .manage(pool)
-        .mount("/", routes![
-            static_files::index, static_files::all,
-        ])
-        .mount("/api/v1/", routes![
-            user::index, user::new, user::show, user::update_by_id, user::delete_by_id,
-            room::index, room::new, room::show, room::update_by_id, room::delete_by_id,
-        ])
+        .mount("/", routes![static_files::index, static_files::all,])
+        .mount(
+            "/api/v1/",
+            routes![
+                user::index,
+                user::new,
+                user::show,
+                user::update_by_id,
+                user::delete_by_id,
+                room::index,
+                room::new,
+                room::show,
+                room::update_by_id,
+                room::delete_by_id,
+            ],
+        )
 }
 
 fn main() {
